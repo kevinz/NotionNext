86f85e209aa67d253b4baceb0af58df567517d26
kevinz <kevintech08@gmail.com>
2024-02-01 17:10:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,9 +18,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'kevin_zz', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://devops.gold', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'devops,kubernetes,platform engineering,aiops,finops', // 网站关键词 英文逗号隔开
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Platform Engineering Architect & PD SpringFramework Author/Linux Kernel contributor/Cloud IAM&RAM designer, K8s/Istio/Knative early advocator.', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.devops.gold', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '平台工程入门,devops,kubernetes,platform engineering,aiops', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
